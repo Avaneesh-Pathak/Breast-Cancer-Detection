c90d804bc4ec8ce56e40078a10c260ab535fd205
Avaneesh-Pathak <avaneeshpathak900@gmail.com>
2023-07-20 13:24:32 +0530

    first commit

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,63 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+# from src.components.model_trainer import ModelTrainerConfig
+# from src.components.model_trainer import ModelTrainer
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path : str=os.path.join('artifact',"train.csv")
+    test_data_path  : str=os.path.join('artifact',"test.csv")
+    raw_data_path   : str=os.path.join('artifact',"data.csv")
+    
+    
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or components")
+        try:
+            df=pd.read_csv(r'D:\Breast_Cancer\Notebook\cleaned_dataset.csv')  
+                      
+            logging.info('Read The datasets as dataframe')
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            
+            logging.info("Train Test Split initiated")
+            
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            
+            logging.info("Ingestion of the data is completed")
+            
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+        
+    data_transformation=DataTransformation()
+    # Call the initiate_data_transformation() method without any arguments
+    train_arr, test_arr, train_target, test_target, preprocessor_path = data_transformation.initiate_data_transformation()    
+    
+    # modeltrainer = ModelTrainer()
+    # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
---
+++ src/components/data_transformation.py
@@ -0,0 +1,93 @@
+import sys
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+# from src.utils import save_obj
+
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        '''
+        This function is responsible for data transformation
+        '''
+        try:
+            numerical_columns = load_breast_cancer().feature_names
+            target_column_name = "target"
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler())
+                ]
+            )
+
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor = ColumnTransformer(
+                transformers=[
+                    ("num_pipeline", num_pipeline, numerical_columns)
+                ]
+            )
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def initiate_data_transformation(self):
+        try:
+            # Load the breast cancer dataset from sklearn
+            data = load_breast_cancer()
+            df = pd.DataFrame(data.data, columns=data.feature_names)
+            df["target"] = data.target
+
+            logging.info("Read data completed")
+
+            logging.info("Obtaining preprocessing object")
+
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_name = "target"
+            numerical_columns = data.feature_names
+
+            input_feature_df = df.drop(columns=[target_column_name])
+            target_feature_df = df[target_column_name]
+
+            logging.info(
+                f"Applying preprocessing object on the dataframe."
+            )
+
+            input_feature_arr = preprocessing_obj.fit_transform(input_feature_df)
+
+            train_arr, test_arr, train_target, test_target = train_test_split(
+                input_feature_arr, target_feature_df, test_size=0.2, random_state=42
+            )
+
+            logging.info(f"Saved preprocessing object.")
+
+            # Save preprocessing_obj using the save_obj function if available
+
+            return (
+                train_arr,
+                test_arr,
+                train_target,
+                test_target,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
